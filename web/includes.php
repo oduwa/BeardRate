d3185a776b1d9e8221c9feb14cce48e606870de7
Odie Edo-Osagie <O.Edo-Osagie@uea.ac.uk>
2016-03-22 12:12:16 +0000

    changed default index.php

---
+++ web/includes.php
@@ -0,0 +1,10 @@
+<!-- JQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<link rel='stylesheet' type='text/css' href='http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css'/>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/thumbnail-gallery.css" rel="stylesheet">
